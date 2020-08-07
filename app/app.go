0fc0bc7fdbeaf34da749eee1a4b259d5f3527655
DivyaVashistha <divyavashishtha23@gmail.com>
2020-08-07 14:30:29 +0530

    [api] feature #4 adding api initialising functions as controller

---
+++ app/app.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/api/app/handler"
+	"github.com/api/app/model"
+	"github.com/api/config"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+// App struct defines router and database
+type App struct{
+	Router *mux.Router
+	DB *gorm.DB
+}
+
+func (a *App) Initialize(config *config.Config){
+	dbURI:=fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True",
+config.DB.Username,
+config.DB.Password,
+config.DB.Host,
+config.DB.Port,
+config.DB.Name,
+config.DB.Charset)
+db,err:=gorm.Open(config.DB.Dialect,dbURI)
+if err!= nil{
+	log.Fatal("could not connect to database")
+}
+a.DB=model.DBMigrate(db)
+a.Router=mux.NewRouter()
+a.setRouters()
+}
+
+// setRouters sets all the required routes
+func (a *App) setRouters(){
+	a.Get("/topics",a.handleRequest(handler.GetAllTopics))
+	a.Post("/users", a.handleRequest(handler.CreateUser))
+}
+
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)){
+	a.Router.HandleFunc(path,f).Methods("GET")
+}
+
+func (a *App) Post(path string,f func(w http.ResponseWriter,r *http.Request)){
+	a.Router.HandleFunc(path,f).Methods("POST")
+}
+
+type RequestHandlerFunction func(db *gorm.DB,w http.ResponseWriter, r *http.Request)
+
+func (a *App) handleRequest(handler RequestHandlerFunction) http.HandlerFunc{
+	return func(w http.ResponseWriter,r *http.Request){
+		handler(a.DB,w,r)
+	}
+}

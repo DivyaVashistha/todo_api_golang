985de289220e30edd78f7199aa9ec682d7b8a07d
DivyaVashistha <divyavashishtha23@gmail.com>
2020-08-07 16:07:46 +0530

    [model] fix #2 updating models for correct foreign key contraints

--- app/model/model.go
+++ app/model/model.go
@@ -10,17 +10,19 @@ type User struct{
 	gorm.Model
 	UserName string `gorm:"unique" json:"user_name"`
 	Password string `gorm:"not null" json:"password"`
-	Topics []Topics `json:"topics"`
+	Topics []Topics `gorm:"ForeignKey:UserID" json:"topics"`
 }
 // Topics struct for defining topic fields
 type Topics struct{
 	gorm.Model
+	UserID uint `json:"user_id"`
 	Title string `gorm:"unique;not null" json:"title"`
-	Details []Details `json:"details"`
+	Details []Details `gorm:"ForeignKey:TopicID" json:"details"`
 }
 // Details struct for defining details of a topic
 type Details struct{
 	gorm.Model
+	TopicID uint `json:"topic_id"`
 	Notes string `gorm:"not null" json:"notes"`
 	Links string `json:"links"`
 	IsComplete bool `json:"is_complete"`
@@ -38,7 +40,7 @@ func (d *Details) WorkInProgressTask(){
 
 func DBMigrate(db *gorm.DB) *gorm.DB{
 	db.AutoMigrate(&User{},&Topics{},&Details{})
-	db.Model(&Details{}).AddForeignKey("topics_id","topics(id)","CASCADE","CASCADE")
 	db.Model(&Topics{}).AddForeignKey("user_id","users(id)","CASCADE","CASCADE")
+	db.Model(&Details{}).AddForeignKey("topic_id","topics(id)","CASCADE","CASCADE")	
 	return db
 }
